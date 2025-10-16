f7d63c85fca668d1a2f364de1e8484698da95f46
matin <matinjalalitekiyeh@gmail.com>
2025-10-16 10:02:23 -0400

    [UPDATE]: load back|front APIs declared for parser

--- app/src/json_parser.hxx
+++ app/src/json_parser.hxx
@@ -1,7 +1,32 @@
 #ifndef JSON_PARSER_HXX
 #define JSON_PARSER_HXX
 
+#include <QObject>
+#include <QVector>
+#include <QString>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QFile>
+
 namespace dqc {
+
+class json_parser
+    : public QObject {
+    Q_OBJECT
+public:
+    explicit
+        json_parser(QObject *parent = nullptr);
+    ~json_parser() override = default;
+
+    json_parser(const json_parser&) = delete;
+    json_parser& operator=(const json_parser&) = delete;
+    json_parser(const json_parser&&) = delete;
+    json_parser& operator=(const json_parser&&) = delete;
+
+    Q_INVOKABLE bool load_backend_config(const QString& config_path);
+    Q_INVOKABLE bool load_frontend_config(const QString& config_path);
+};
 
 }
 
