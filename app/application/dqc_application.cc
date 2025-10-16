922825ac4fcbefc23e201882f412e97f2812a191
matin <matinjalalitekiyeh@gmail.com>
2025-10-16 03:02:58 -0400

    [ADD]: Add dqc application as facade

---
+++ app/application/dqc_application.cc
@@ -0,0 +1,5 @@
+#include "dqc_application.hxx"
+
+namespace dqc {
+
+}
---
+++ app/application/dqc_application.hxx
@@ -0,0 +1,8 @@
+#ifndef DQC_APPLICATION_HXX
+#define DQC_APPLICATION_HXX
+
+namespace dqc {
+
+}
+
+#endif /* DQC_APPLICATION_HXX */
