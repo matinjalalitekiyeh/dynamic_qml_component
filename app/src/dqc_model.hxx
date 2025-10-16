a46938c7fa0567e01b94524851003f1369317a4a
matin <matinjalalitekiyeh@gmail.com>
2025-10-16 08:33:56 -0400

    [UPDATE]: inh from qabstract-model-list to use repeater for my dqcs

--- app/src/dqc_model.hxx
+++ app/src/dqc_model.hxx
@@ -1,7 +1,49 @@
 #ifndef DQC_MODEL_HXX
 #define DQC_MODEL_HXX
 
+#include <QAbstractListModel>
+#include "dqc_proprty.hxx"
+
 namespace dqc {
+
+class DQC_Roles {
+    Q_GADGET
+public:
+    enum class roles {
+        Data_source = Qt::UserRole + 1,
+        Color,
+        Initial_x,
+        Initial_y,
+        Interval,
+        Random_num,
+        Random_from,
+        Random_to
+    };
+    Q_ENUM(roles)
+};
+
+class dqc_model
+    : public QAbstractListModel {
+    Q_OBJECT
+public:
+    explicit
+        dqc_model(QObject *parent = nullptr);
+
+    /*override.*/
+    int rowCount([[__maybe_unused__]]const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+    bool setData(const QModelIndex &index,
+                 const QVariant &value, int role) override;
+    Qt::ItemFlags flags(const QModelIndex& index) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+    Q_INVOKABLE void add_dqc_rect(dqc::dqc_rect_properties *property);
+    Q_INVOKABLE void add_dqc_rect();
+    Q_INVOKABLE void remove_dqc_rect(const int index);
+
+private:
+    QList<dqc::dqc_rect_properties*> m_dqc_rect_properties;
+};
 
 }
 
