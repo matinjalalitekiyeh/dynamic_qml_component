447ad591f15cf9ba6cf783ef4d4a6851c6ad2e67
matin <matinjalalitekiyeh@gmail.com>
2025-10-16 11:45:24 -0400

    [UPDATE]: Potential mem leak

--- app/application/dqc_application.cc
+++ app/application/dqc_application.cc
@@ -40,13 +40,10 @@ void dqc_application::post_init()
     auto backend = m_parser.load_backend_config("backend_3.json");
     auto frontend = m_parser.load_frontend_config("frontend.json");
 
-    // qDebug() << "------------------------------------------------------------";
-    auto *property = new dqc::dqc_rect_properties(this);
     for (const auto &f : std::as_const(frontend)) {
         for (const auto &b : std::as_const(backend)) {
-            // qDebug() << f.data_source << b.id;
             if (f.data_source == b.id) {
-
+                auto *property = new dqc::dqc_rect_properties(this);
                 property->set_data_source(f.data_source);
                 property->set_color(f.color);
                 property->set_initial_x(f.x);
@@ -54,22 +51,10 @@ void dqc_application::post_init()
                 property->set_interval(b.msec);
                 property->set_random_from(b.min);
                 property->set_random_to(b.max);
-                // property->set_random_num(b.msec);
-                qDebug() << b.msec;
-
                 m_model.add_dqc_rect(property);
-                // qDebug() << "**********FOUND*******FOUND******FOUND***********************";
             }
         }
-        // qDebug() << "**************";
     }
-
-    // property
-
-
-    // for (int i = 0; i < 20; i++) {
-    //     m_model.add_dqc_rect();
-    // }
 }
 
 }
